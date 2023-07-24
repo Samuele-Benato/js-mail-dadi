7133b03eb5348c371f8adcea85f325a7d09af70a
Samuele-Benato <samuele.benato@gmail.com>
2023-07-25 01:15:36 +0200

    fix : add mail style

--- MAIL/js/main.js
+++ MAIL/js/main.js
@@ -1,13 +1,41 @@
 // array con le mail possibili
-const mails = ["@gmail.com", "@outlook.com", "@yahoo.com"];
-// chiedere mail
-const userMail = prompt("inserisci la mail");
+const mails = [
+  "Goku@gmail.com",
+  "Vegeta@gmail.com",
+  "Crillin@gmail.com",
+  "Gohan@gmail.com",
+  "MrSatan@gmail.com",
+  "C-16@gmail.com",
+  "C-17@gmail.com",
+  "C-18@gmail.com",
+  "Bulma@gmail.com",
+  "MajinBu@gmail.com",
+  "Freezer@gmail.com",
+  "Trunks@gmail.com",
+  "Cell@gmail.com",
+  "Chichi@gmail.com",
+  "Goten@gmail.com",
+  "MaestroMuten@gmail.com",
+  "Shenron@gmail.com",
+  "Nappa@gmail.com",
+  "Tenshing@gmail.com",
+  "Dende@gmail.com",
+  "Darbula@gmail.com",
+  "Ub@gmail.com",
+  "Zarbon@gmail.com",
+];
+// definisco l'user mail
+let userMail = prompt(
+  "Provaci! Inserisci la mail del personaggio più iconico di DragonBall Z  (Lettera maiuscola iniziale di ogni parola + '@gmail.com')"
+);
+
+// // definisco il value dell'userMail
+// let userMailValue = document.getElementById("user_mail").value;
 
 let correct = false;
 // definire ogni mail possibile
 for (let i = 0; i < mails.length; i++) {
   const mail = mails[i];
-  console.log(mail);
 
   // se la mail corrisponde a quelle elencate è compatibile
   if (userMail === mail) {
@@ -16,7 +44,13 @@ for (let i = 0; i < mails.length; i++) {
 }
 
 if (correct) {
-  alert("mail inserita con successo");
+  let mailCorrect = document.getElementById("mail_correct");
+  mailCorrect.classList.remove("d-none");
 } else {
-  alert("mail non valida");
+  let mailFail = document.getElementById("mail_fail");
+  mailFail.classList.remove("d-none");
 }
+
+// // richiamo il bottone dall'html
+// const myButton = document.getElementById("btn_newsletter");
+// myButton.addEventListener("click", function () {});
