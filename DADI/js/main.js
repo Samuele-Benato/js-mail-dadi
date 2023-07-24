3eb2c18fab1b6dc33cf16027ea54dcf88d6a2339
Samuele-Benato <samuele.benato@gmail.com>
2023-07-24 21:42:42 +0200

    feat : add style

--- DADI/js/main.js
+++ DADI/js/main.js
@@ -10,15 +10,27 @@ let random2 = Math.floor(Math.random() * maxNumber) + minNumber;
 
 // dichiaro numero dell'user
 let userNumber = random1;
-console.log(random1);
 // dichiaro numero del computer
 let computerNumber = random2;
-console.log(random2);
 
-if (userNumber < computerNumber) {
-  alert("complimenti USER hai vinto!");
-} else if (userNumber == computerNumber) {
-  alert("ACCIDENTI parità!");
-} else {
-  alert("AHAH ti sei fatto battere da un computer!");
-}
+// richiamo il bottone dall'html
+const myButton = document.getElementById("btn_gioca");
+myButton.addEventListener("click", function () {
+  // scrivo il risutato in tabella
+  let userNumberGenerator = document.getElementById("user_number");
+  userNumberGenerator.innerHTML = userNumber;
+
+  // scrivo il risutato in tabella
+  let computerNumberGenerator = document.getElementById("computer_number");
+  computerNumberGenerator.innerHTML = computerNumber;
+
+  let messageUser = document.getElementById("message");
+
+  if (userNumber < computerNumber) {
+    messageUser.innerHTML = "Che fortuna! HAI VINTO.";
+  } else if (userNumber == computerNumber) {
+    messageUser.innerHTML = "ACCIDENTI! parità.";
+  } else {
+    messageUser.innerHTML = "Che peccato! HAI PERSO";
+  }
+});
